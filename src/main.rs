748c041f61fc42dc3dd4a8687c10c05570abb6a0
Patrick Murphy <patmurf@live.com>
2025-06-09 17:18:55 -0500

    ensuring tests work in pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+pub fn hello_world() -> String {
+    "Hello, Kill The Kings!"
+}
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_hello_world() {
+        assert_eq!("Hello, Kill The Kings!", hello_world());
+    }
 }
